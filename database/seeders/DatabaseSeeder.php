7bc2492516e5a33d6621dba77623990cbb69079e
Magatte Diene <papemagattendiayediene@gmail.com>
2026-07-14 23:26:06 +0000

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,22 @@ class DatabaseSeeder extends Seeder
 
     /**
      * Seed the application's database.
+     *
+     * RoleSeeder est structurel (les 5 roles fixes de l'app) : toujours
+     * execute, y compris en production. UtilisateurSeeder/SessionInventaireSeeder
+     * ne creent que des donnees de demo/dev (comptes a mot de passe partage,
+     * fausses sessions) : jamais executes en production, pour que
+     * `php artisan db:seed` reste la meme commande partout sans risque.
      */
     public function run(): void
     {
-        $this->call([
-            RoleSeeder::class,
-            UtilisateurSeeder::class,
-            SessionInventaireSeeder::class,
-        ]);
+        $this->call(RoleSeeder::class);
+
+        if (!app()->environment('production')) {
+            $this->call([
+                UtilisateurSeeder::class,
+                SessionInventaireSeeder::class,
+            ]);
+        }
     }
 }
